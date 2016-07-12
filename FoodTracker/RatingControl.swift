552f86b2f69060483051a8a2ab615aa143509ef6
Vu Le <lbkhanhvu@gmail.com>
2016-07-12 23:42:46 +0900

    add RatingControl.swift

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,18 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by William on 2016/07/12.
+//  Copyright © 2016年 William. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIView {
+
+    // MARK: Initialization
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+}
